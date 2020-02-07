50999286ee1f97d54b6b7cab1e5ee0312fceff80
Xuantho01 <hoxuantho95@gmail.com>
2020-02-08 00:23:25 +0700

    update animation

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 let test = true;
 document.getElementById("dialog").style.display = "none";
-// while(1){
-//     document.getElementById("dialog").style.display = 'none';
-// }
+// Text effect
 setInterval( function(){
     document.getElementById("note").style.display = "none";
     setTimeout( function(){
         document.getElementById("note").style.display = "block";
     },500);
 },1000);
-// lớp hình ảnh
+let a;
+function animation() {
+    a = setInterval(function () {
+            document.getElementById("point").style.display = "none";
+            setTimeout(function () {
+                document.getElementById("point").style.display = "block";
+            }, 100);
+        }, 200);
+}
+function clear(){
+    clearInterval(a);
+}
+// for(let i = 0; i < 10; i++){
+//        // setInterval( function(){
+//         document.getElementById("point").style.display = "none";
+//         setTimeout( function(){
+//             document.getElementById("point").style.display = "block";
+//         },1000);
+//    // },1000);
+// }
+// class Images
 let Image = function(arrImage) {
     this.arrImages = arrImage;
     this.count = 0;
@@ -29,26 +47,38 @@ let Image = function(arrImage) {
         this.setImage(this.arrImages[this.count]);
         document.getElementById("image").src = this.getImage();
     };
-    // this.setCountToZero = function(){
-    //    return this.count = 0;
-    // }
 };
-// lớp câu trả lời
+// Class Answer
 let Answers = function(arrAnswers) {
-    // this.getAnswer = function(){
-    //     return this.Answer;
-    // };
     this.arrAnswers = arrAnswers;
     let tmp = this;
     this.setAnswer = function () {
-        this.Answer = document.getElementById("Answer").value;
+            this.Answer = document.getElementById("Answer").value;
+        };
+    this.checkInput = function() {
+        if (this.Answer == "ném đá dấu tay" || this.value == "NEM DA DAU TAY" || this.value == "NÉM ĐÁ DẤU TAY" || "Ném đá dấu tay") {
+            this.Answer = "0";
+        } else if (this.Answer == "nhạc cụ" || this.Answer == "NHẠC CỤ" || this.Answer == "Nhạc cụ" || this.Answer == "NHAC CU") {
+            this.Answer = "1";
+        } else if (this.Answer == "c" || this.Answer == "C") {
+            this.Answer = "2";
+        } else if (this.Answer == "D" || this.Answer == "d") {
+            this.Answer = "3";
+        }
+        else{
+            this.Answer = "";
+        }
+    };
+    this.getAnswer = function(){
+        return this.Answer;
     };
     this.checkAnswer = function () {
         if(test){
             this.setAnswer();
+            this.checkInput();
             point.setTurn();
             point.getTurn();
-            if (this.Answer == this.arrAnswers[image.count]) {
+            if (this.getAnswer() == this.arrAnswers[image.count]) {
                 document.getElementById("notification").innerHTML = "<br>" + 'Chúc mừng bạn!'+ "<br>"
                         + "<img style='width: 200px; height: 200px' " + "src='ima/like.gif'>";
                 audio.src = 'audio/true.mp3';
@@ -56,6 +86,11 @@ let Answers = function(arrAnswers) {
                 image.changeImage();
                 point.setPoint();
                 point.getPoint();
+                animation();
+                setTimeout(function(){
+                    clear();
+                    },400);
+                // animation();
                 document.getElementById("Answer").value = null;
                 if (point.point == 40) //need to update
                 {
@@ -63,13 +98,12 @@ let Answers = function(arrAnswers) {
                     document.getElementById("dialog").style.display = 'block';
                     test = false;
                     audio.src = 'audio/fire.mp3';
-                    setInterval(function (){
-                        audio.play()
-                    },1000);
-                    //b('ima/fire.mp3').play();
-                    //setInterval(audio.play,4000);
+                    setTimeout(function (){
+                        clear();
+                    },300);
                 }
-            } else if (this.Answer == "") {
+            }
+            else if (this.getAnswer() == "") {
                 alert("Bạn chưa nhập câu trả lời!");
                 return document.getElementById("notification").innerHTML = "<br>" + "Chưa nhập câu trả lời!" + "<br>" + "<br>" +
                     "<img style='width: 130px; height: 130px' " +
@@ -98,7 +132,7 @@ let Answers = function(arrAnswers) {
             "src='https://media1.tenor.com/images/c3324236b32e659783734fe2813603c8/tenor.gif?itemid=15112797'>";
     };
 };
-// lớp điểm
+// Class Point
 let Points = function(point,turn,lose){
     this.point = point;
     this.turn = turn;
@@ -135,25 +169,19 @@ let Points = function(point,turn,lose){
     };
    // this.a = false;
     this.resetResults = function() {
-       // this.a = true;
-       // image.count = 0;
          document.getElementById("image").src = arrImages[0];
          this.reset();
          test = true;
     }
 };
-// function PlaySound(filename){
-//     let mp3Source = '<source src="' + filename + '.mp3">';
-//     // var oggSource = '<source src="' + filename + '.ogg" type="audio/ogg">';
-//     let embedSource = '<embed hidden="true" autostart="true" loop="true" src="' + filename +'.mp3">';
-//     document.getElementById("sound").innerHTML='<audio autoplay="autoplay">' + mp3Source + '</audio>';
-// }
 let arrImages = ["https://files.vforum.vn/2014/T10/img/vforum.vn-133676-10309683-755442207841364-361679200786066980-n.jpg",
               "https://files.vforum.vn/2014/T10/img/vforum.vn-133676-1011229-473268726140798-1607420842666641281-n.jpg",
               "https://files.vforum.vn/2014/T10/img/vforum.vn-133676-10262185-473271902807147-7738376419430735243-n.jpg",
               "https://files.vforum.vn/2014/T10/img/vforum.vn-133676-10402396-755442184508033-1390713449637084439-n.jpg"];
-let arrAnswers = ["ném đá dấu tay","b","c","d"];
+let arrAnswers = ["0","1","2","3"];
 let image = new Image(arrImages);
 let answers = new Answers(arrAnswers);
 let point = new Points(0,0,0);
+//let Check_spelling = new checkInput(value);
 let audio = new Audio();
+
